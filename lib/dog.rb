36b6dc373b2a4eb89c329c8a1dc726290e9fed3c
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-03 04:38:13 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,54 @@
+class Dog
+
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT)
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL 
+    DROP TABLE dogs
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+  end
+  
+  
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+  
+   def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+  
+end
